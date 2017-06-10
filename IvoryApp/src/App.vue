23cbb4486b64cc79d34fe7d9f34f89563e02523f
happylrd <happylrd17@gmail.com>
2017-06-10 21:06:55 +0800

    Add muse ui and config theme

--- IvoryApp/src/App.vue
+++ IvoryApp/src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <mu-appbar title="Ivory">
+      <mu-icon-button icon="menu" slot="left"/>
+      <mu-flat-button label="测试" slot="right"/>
+      <mu-flat-button href="https://happylrd.github.io/" label="happylrd的博客" slot="right"/>
+      <mu-icon-button icon="expand_more" slot="right"/>
+    </mu-appbar>
   </div>
 </template>
 
--- IvoryApp/src/main.js
+++ IvoryApp/src/main.js
@@ -2,9 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+import 'muse-ui/dist/theme-light.css'
 // import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(MuseUI)
 
 /* eslint-disable no-new */
 new Vue({
